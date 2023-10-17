05073bb64f43b408431f79234f09ee0ebeb4b55a
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-10-17 00:29:14 +0000

    init and tutorial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    //a very basic clicker used for our svelte kit demo.
+    //the logic that defines clicks per click and handles the actual click
+    let clicksPerClick = 1;
+    let clicks = 0;
+    function buttonClick() {
+        clicks += clicksPerClick;
+    }
+</script>
+
+<h1>Click:</h1>
+
+<button on:click={buttonClick}>Click Me</button>
+<p>Balance: {clicks}</p>
+
+<h1>Upgrades:</h1>
